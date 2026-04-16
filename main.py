1d8445e77e10eace5cd783dc5996ce86197ef75a
adnannasution <adnan.buyung01@gmail.com>
2026-04-16 10:48:56 +0700

    update versi terbaru langchai terbaru

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ from models import Base, KPIData
 from langchain_openai import ChatOpenAI
 from langchain_community.utilities import SQLDatabase
 from langchain_experimental.sql import SQLDatabaseChain
-from langchain.prompts import PromptTemplate
+from langchain_core.prompts import PromptTemplate
 from dotenv import load_dotenv
 
 load_dotenv()
